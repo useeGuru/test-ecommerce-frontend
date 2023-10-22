521480a4d95d8b202dbfe0f3f380170dfc101e1c
useeGuru <skybusiness1107@gmail.com>
2023-10-21 20:14:55 -0700

    Update backend url

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-  baseURL: process.env.REACT_APP_BASE_URL,
-};
-
-export default config;
--- src/request-methods.js
+++ src/request-methods.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
-import config from './config';
 
-const BASE_URL = config.baseURL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "";
+console.log(BACKEND_URL, 'BASE_URL')
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGEzOGUwYjczZTg5NTczZDAxNmZmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzA3NTI3OSwiZXhwIjoxNjYzMTYxNjc5fQ.sLeBTfgAymobkEF1rcOzexB8-ixEXKWat-7lRFfaKVQ';
 
 export const publicRequest = axios.create(
   {
-    baseURL: BASE_URL
+    baseURL: BACKEND_URL
   }
 );
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BACKEND_URL,
   headers: {
     'Authorization': `Bearer ${TOKEN}`
   }
