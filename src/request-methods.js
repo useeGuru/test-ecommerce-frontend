45792fff1932897ba21e9ea4613e5d3a88ae225f
useeGuru <skybusiness1107@gmail.com>
2023-10-21 19:27:51 -0700

    update api url

--- src/request-methods.js
+++ src/request-methods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://railway.app/project/e5c0849c-61bf-433a-87f3-b6ea688cfe97/service/a1d1d61f-3cad-44ab-ae93-7b02495366a4';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGEzOGUwYjczZTg5NTczZDAxNmZmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzA3NTI3OSwiZXhwIjoxNjYzMTYxNjc5fQ.sLeBTfgAymobkEF1rcOzexB8-ixEXKWat-7lRFfaKVQ';
 
 export const publicRequest = axios.create(
